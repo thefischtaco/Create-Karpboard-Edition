e4fcc853ace839df75d7629fbd788714dbbce717
ghoti <42419812+thefischtaco@users.noreply.github.com>
2025-12-08 14:19:08 -0600

    Add files via upload

---
+++ kubejs/server_scripts/Removal.js
@@ -0,0 +1,49 @@
+// priority: 0
+ServerEvents.recipes(event => {
+
+    event.remove({output: 'minecraft:blast_furnace'})
+    event.remove({id: 'minecraft:iron_ingot_from_smelting_raw_iron'})
+
+//==========================================================================================================  
+   
+    event.remove({output: 'easy_mob_farm:enduring_capture_net'})
+    event.remove({output: 'easy_mob_farm:ironbound_containment_cage'})
+    event.remove({output: 'easy_mob_farm:mystic_binding_crystal'})
+    event.remove({output: 'easy_mob_farm:void_binding_chain'})
+
+//==========================================================================================================  
+    
+    event.remove({output: 'sophisticatedstorage:storage_link'})
+    event.remove({output: 'sophisticatedstorage:storage_io'})
+    event.remove({output: 'sophisticatedstorage:storage_input'})
+    event.remove({output: 'sophisticatedstorage:storage_output'})
+
+//==========================================================================================================
+
+    event.remove({output: 'tfmg:lithium_blade'})
+    event.remove({output: 'tfmg:flamethrower'})
+    event.remove({output: 'tfmg:advanced_potato_cannon'})
+    event.remove({output: 'tfmg:quad_potato_cannon'})
+    event.remove({output: 'tfmg:pipe_bomb'})
+    event.remove({output: 'tfmg:thermite_grenade'})
+    event.remove({output: 'tfmg:zinc_grenade'})
+    event.remove({output: 'tfmg:copper_grenade'})
+    event.remove({output: 'tfmg:napalm_bomb'})
+    event.remove({output: 'tfmg:napalm_potato'})
+    event.remove({output: 'tfmg:napalm'})
+
+//==========================================================================================================
+    
+    event.remove({id: 'create_things_and_misc:blaze_ballon_fire'})
+
+//===========================================================================================================
+
+    event.remove({output: 'cobblemoncreatified:hp_silver_bottlecap'})
+    event.remove({output: 'cobblemoncreatified:atk_silver_bottlecap'})
+    event.remove({output: 'cobblemoncreatified:def_silver_bottlecap'})
+    event.remove({output: 'cobblemoncreatified:spatk_silver_bottlecap'})
+    event.remove({output: 'cobblemoncreatified:spdef_silver_bottlecap'})
+    event.remove({output: 'cobblemoncreatified:speed_silver_bottlecap'})
+
+
+})
