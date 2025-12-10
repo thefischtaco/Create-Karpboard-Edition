1b50f4c76ef66547c1fd03893d8143e09a039ccf
HSavageheart <47013090+HSavageheart@users.noreply.github.com>
2025-12-10 20:44:52 +0100

    Add files via upload

---
+++ kubejs/server_scripts/disable_masterball_trial.js
@@ -0,0 +1,5 @@
+// Visit the wiki for more info - https://kubejs.com/
+BlockEvents.rightClicked('minecraft:trial_spawner', event => event.cancel()),
+BlockEvents.rightClicked('cobblemoncreatified:pokemon_trial_spawner', event => event.cancel()),
+console.info('Hello, World! (Loaded server example script)')
+
